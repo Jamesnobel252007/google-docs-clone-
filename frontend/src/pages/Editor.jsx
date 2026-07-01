49a538f99d55193207ac0060d271e5970cbd512b
Jamesnobel252007 <nobellourdu@gmail.com>
2026-07-01 15:50:54 +0530

    login

--- frontend/src/pages/Editor.jsx
+++ frontend/src/pages/Editor.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import * as XLSX from "xlsx";
+
 import TiptapEditor from "../components/TiptapEditor";
 import api from "../api/api";
 function Editor() {
